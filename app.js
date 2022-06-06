0d3e4d8549681bbcbcded322915707c4b0a0de26
Julie Nisbet <jmnisbet@gmail.com>
2022-06-06 10:53:44 -0700

    demo code

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
+// app.js
+const { response } = require("express");
 const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
+// Built in middleware
+app.use(express.json());
+
+// app.use("/dogs/:id", (req, res) => {
+//   console.log(req.params.id);
+//   res.json({ name: "Benny", age: 7 });
+// });
+
+// app.use("/dogs", (req, res) => {
+//   res.json([
+//     { name: "Benny", age: 7 },
+//     { name: "Jeep", age: 4 },
+//   ]);
+// });
+
+app.use("/api/v1/dogs", require("./lib/controllers/dogs.js"));
 
 app.listen(3000, () => {
   console.log(`🚀  Server started on Port 3000`);
